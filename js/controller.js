e0c8691a90f7a43f830ad88a576ca2bb1c238ed0
tarunraj95 <tarunrajkhanna@gmail.com>
2018-02-15 17:05:32 +0530

    api call added

--- js/controller.js
+++ js/controller.js
@@ -1,5 +1,8 @@
 app.controller("myController", function($scope, myFactory) {
   console.log("inside controller...");
   $scope.list = "this is the list";
-  myFactory.apiCall();
+  let res = myFactory.apiCall();
+  res.then(function(data) {
+    console.log("data recieved in controller is =", data);
+  });
 });
--- js/factory.js
+++ js/factory.js
@@ -2,6 +2,22 @@ app.factory("myFactory", function($q, $http) {
   return {
     apiCall: function() {
       console.log("inside factory....");
+      let deferedObj = $q.defer();
+
+      var res = $http
+        .get(
+          "https://raw.githubusercontent.com/tarunraj95/Json_data/master/seenit_quests.json"
+        )
+        .then(
+          data => {
+            console.log("json recieved is=", data);
+            deferedObj.resolve(data);
+          },
+          error => {
+            deferedObj.reject(error);
+          }
+        );
+      return deferedObj.promise;
     }
   };
 });
